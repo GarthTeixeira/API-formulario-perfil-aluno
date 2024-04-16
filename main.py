b3415562a6005d6475c48f292d296d3d8aead9b4
GArthTex66 <garthur1998@gmail.com>
2024-04-16 00:49:36 -0300

    enable cors

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, jsonify
 from blueprints.competences.competeces_controller import competences_bp
 from blueprints.disciplines_controller import disciplines_bp
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app)
 app.register_blueprint(competences_bp, url_prefix='/competences')
 app.register_blueprint(disciplines_bp, url_prefix='/disciplines')
 
