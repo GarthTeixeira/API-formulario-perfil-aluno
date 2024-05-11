da1d1cb5b1b3e6bf5310d8b0d508bceb355443b3
GarthTeixeira <garthur1998@gmail.com>
2024-05-11 15:19:19 -0300

    adhyst to httpResponse

--- utils/httpResposeUtils.py
+++ utils/httpResposeUtils.py
@@ -4,8 +4,9 @@ class HttpResponseUtils:
     @staticmethod
     def responseForAPIFromArrayData(arrayData):
         for data in arrayData:
-            id = data.pop('_id')
-            data['id'] = str(id)
+            if '_id' in data:
+                id = data.pop('_id')
+                data['id'] = str(id)
     
         return make_response(arrayData)
     
