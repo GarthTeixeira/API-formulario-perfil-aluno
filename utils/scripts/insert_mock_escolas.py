9529b9ab4d1325f0586a825e24c3ba5efaf9616f
GarthTeixeira <garthur1998@gmail.com>
2024-09-29 20:29:12 -0300

    Alteração de scripts de inserção de escolar, contendo mock de notas do histórico escolar

--- utils/scripts/insert_mock_escolas.py
+++ utils/scripts/insert_mock_escolas.py
@@ -3,8 +3,12 @@ from pymongo import MongoClient
 from urllib.parse import quote_plus
 import json
 import copy
+import random
+import os
 
-f_config = open('../../db_resources/db_config.json')
+print("Current Working Directory:", os.getcwd())
+
+f_config = open('/home/garth/Documents/Projetos/API-formulario-perfil-aluno/db_resources/db_config.json')
 mongo_db_infos = json.load(f_config)
 
 
@@ -14,8 +18,8 @@ connection_string = 'mongodb+srv://{}:{}@{}/?retryWrites=true&w=majority&appName
             mongo_db_infos['HOST']
         )
 
-f_disciplinas = open('../mock/disciplinas.json')
-f_escolas = open('../mock/escolas.json')
+f_disciplinas = open('{}/utils/mock/disciplinas.json'.format(os.getcwd()))
+f_escolas = open('{}/utils/mock/escolas.json'.format(os.getcwd()))
 
 # Connect to the database
 client = MongoClient(connection_string)
@@ -35,12 +39,32 @@ for escola in escolas:
         escola['disciplinas'].append(disciplina)
     
     turmas = escola['turmas']
+    
 
     for turma in turmas:
         for serie in series:
             nova_turma = copy.deepcopy(turma)
             nova_turma["serie"] = serie
             nova_turma['_id'] = ObjectId()
+            serie_num = int(serie[0])
+
+            disciplinas_from_serie = list(filter(
+                lambda disciplina: disciplina['serie_ano'] == serie_num,
+                escola['disciplinas']
+            ))
+
+            for aluno in nova_turma['alunos']:
+                ## Generate random grade for student and add to scholar historic
+                novo_historico = [
+                    {
+                        "disciplina_id":d["_id"],
+                        "disciplina_titulo":d["name"] + "-" + str(d["serie_ano"]),
+                        "nota": round(random.uniform(0, 10), 1)
+                    } 
+                    for d in disciplinas_from_serie
+                ]
+                aluno['notas']['historico_escolar'] = aluno['notas']['historico_escolar'] + novo_historico
+
             novas_turmas.append(nova_turma)
 
     escola['turmas'] = novas_turmas
@@ -50,5 +74,6 @@ for escola in escolas:
 # Insert all the data into the collection
 collection.insert_many(escolas)
 
+print('Inserção realizada com sucesso!')
 # Close the MongoDB connection
 client.close()
