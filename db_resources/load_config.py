1512992bee1a61aafb3c568ef510c5ca44e8b4fa
GarthTeixeira <garthur1998@gmail.com>
2025-02-07 14:24:16 -0300

    add logic to non-existem db_config

--- db_resources/load_config.py
+++ db_resources/load_config.py
@@ -7,8 +7,30 @@ load_dotenv()
 
 # Função para carregar configurações de um arquivo JSON
 def load_config(json_path="db_resources/db_config.json"):
-    with open(json_path, "r") as file:
-        config = json.load(file)[os.getenv("FLASK_ENV")]
+    # Configurações padrão (caso o arquivo não exista)
+    default_config = {
+        "HOST": "",
+        "PORT": "",
+        "SRV": "",
+        "USER": "",
+        "PASSWORD": "",
+        "DB_NAME": "",
+        "PARAMS": ""
+    }
+
+    if os.path.exists(json_path):
+        try:
+            with open(json_path, "r") as file:
+                config = json.load(file)[os.getenv("FLASK_ENV")]
+        except Exception as e:
+            print(f"Erro ao carregar o arquivo de configuração: {e}")
+            config = {}
+    else:
+        print(f"Arquivo de configuração não encontrado: {json_path}")
+        config = {}
+    
+    # Mescla as configurações padrão com as do arquivo JSON
+    config = {**default_config, **config}
 
     # Sobrescrever variáveis com valores do ambiente, se definidos
     host = os.getenv("HOST", config["HOST"])
