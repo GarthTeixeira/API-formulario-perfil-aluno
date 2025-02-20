c8114b2dd8b7908d3d4e08666d1e4c37a293647c
GarthTeixeira <garthur1998@gmail.com>
2025-02-20 15:42:11 -0300

    insert for school and record

--- utils/scripts/insert_escolas.py
+++ utils/scripts/insert_escolas.py
@@ -1,82 +1,113 @@
-# TODO: Deprecated!!! update script to make more sense
+"""
+Script será usado para inserir todas as escolas em um ambiente, não inserindo alunos. Primeiro passo para uma funcionalidade padrão de produção
+Certifique-se que as turmas estão preenchidaas corretamente com os alunos, após a inserção crie um objeto para o histórico escolar, associando as notas as disciplinas para um aluno.
+"""
 
 from bson.objectid import ObjectId
 from pymongo import MongoClient
 from urllib.parse import quote_plus
 import json
 import copy
-import random
 import os
+import argparse
 
-print("Current Working Directory:", os.getcwd())
+#Caminho Local
+local_path = os.path.dirname(__file__)
+print("Local path",local_path)
+# Caminho relativo até "db_resources"
+db_resources_path = os.path.join(local_path, '..', '..', 'db_resources')
+# Normalizando o caminho
+db_resources_path = os.path.abspath(db_resources_path)
 
-f_config = open('/home/garth/Documents/Projetos/API-formulario-perfil-aluno/db_resources/db_config.json')
+parser = argparse.ArgumentParser(description="Insere escola no banco de dados.")
+
+parser.add_argument("--school", type=str, default="", help="Nome da escola")
+parser.add_argument("--env", type=str, default="", help="ambiente")
+
+# Parse dos argumentos
+args = parser.parse_args()
+f_config = {}
+
+f_config = open('{}/db_config.json'.format(db_resources_path))
 mongo_db_infos = json.load(f_config)
 
+mongo_db_infos = mongo_db_infos[args.env]
 
-connection_string = 'mongodb+srv://{}:{}@{}/?retryWrites=true&w=majority&appName=Cluster0&ssl=true'.format(
-            mongo_db_infos['USERNAME'],
+if 'CLUSTER' not in mongo_db_infos:
+    mongo_db_infos['CLUSTER'] = mongo_db_infos['HOST'] + ":" + mongo_db_infos['PORT']
+
+connection_string = 'mongodb{}://{}:{}@{}/{}'.format(
+            mongo_db_infos['SRV'],
+            mongo_db_infos['USER'],
             quote_plus(mongo_db_infos['PASSWORD']),
-            mongo_db_infos['HOST']
+            mongo_db_infos['CLUSTER'],
+            mongo_db_infos['PARAMS']
         )
 
-f_disciplinas = open('{}/utils/mock/disciplinas.if.json'.format(os.getcwd()))
-f_escolas = open('{}/utils/mock/escolas.json'.format(os.getcwd()))
+f_disciplinas = open('{}/data/disciplinas.{}.json'.format(local_path, args.school))
+f_escolas =  open('{}/data/escolas.{}.json'.format(local_path, args.school))
 
 # Connect to the database
 client = MongoClient(connection_string)
 db = client['competencias_enem_data']
 collection = db['escolas']
 
-escolas = json.load(f_escolas)
+escola = json.load(f_escolas)
 disciplinas = json.load(f_disciplinas)
+
+result = collection.delete_one({'name':escola['name']})
 
 series = ["1º ano", "2º ano", "3º ano"]
 
-for escola in escolas:
-    novas_turmas = []
-    escola['_id'] = ObjectId()
-    for disciplina in disciplinas:
-        nova_disciplina = copy.deepcopy(disciplina)
-        nova_disciplina['_id'] = ObjectId()
-        escola['disciplinas'].append(nova_disciplina)
-    
-    turmas = escola['turmas']
-    
+novas_turmas = []
+escola['_id'] = ObjectId()
+for disciplina in disciplinas:
+    nova_disciplina = copy.deepcopy(disciplina)
+    nova_disciplina['_id'] = ObjectId()
+    escola['disciplinas'].append(nova_disciplina)
 
-    for turma in turmas:
-        for serie in series:
-            nova_turma = copy.deepcopy(turma)
-            nova_turma["serie"] = serie
-            nova_turma['_id'] = ObjectId()
-            serie_num = int(serie[0])
+turmas = escola['turmas']
 
-            disciplinas_from_serie = list(filter(
-                lambda disciplina: disciplina['serie_ano'] == serie_num,
-                escola['disciplinas']
-            ))
+historicos = {}
 
-            for aluno in nova_turma['alunos']:
-                ## Generate random grade for student and add to scholar historic
-                novo_historico = [
+for turma in turmas:
+    for serie in series:
+        nova_turma = copy.deepcopy(turma)
+        nova_turma["serie"] = serie
+        nova_turma['_id'] = ObjectId()
+        serie_num = int(serie[0])
+        nova_turma['ano'] = nova_turma['ano'] + serie_num -1
+
+        disciplinas_from_serie = list(filter(
+            lambda disciplina: disciplina['serie_ano'] == serie_num,
+            escola['disciplinas']
+        ))
+
+        novo_historico = [
                     {
-                        "disciplina_id":d["_id"],
+                        "disciplina_id":str(d["_id"]),
                         "disciplina_titulo":d["name"] + "-" + str(d["serie_ano"]),
-                        "nota": round(random.uniform(0, 10), 1)
+                        "nota": 0
                     } 
                     for d in disciplinas_from_serie
                 ]
-                aluno['notas']['historico_escolar'] = aluno['notas']['historico_escolar'] + novo_historico
-                aluno['_id'] = ObjectId()
-            novas_turmas.append(nova_turma)
+        
+        historicos[serie] = novo_historico
+        novas_turmas.append(nova_turma)
 
-    escola['turmas'] = novas_turmas
-
-
+escola['turmas'] = novas_turmas
 
 # Insert all the data into the collection
-collection.insert_many(escolas)
+shcools_inserted = collection.insert_one(escola)
 
 print('Inserção realizada com sucesso!')
 # Close the MongoDB connection
 client.close()
+
+print(historicos)
+file_name = "modelo_historico.{}.json".format(args.school)
+
+with open("./data/{}".format(file_name), "w",  encoding="utf-8") as json_file:
+    json.dump(historicos, json_file,  ensure_ascii=False, indent=4) 
+
+print("{} created!".format(file_name))
---
+++ utils/scripts/insert_historico.py
@@ -0,0 +1,87 @@
+"""
+Script que utiliza o modelo de histórico para fazer inserção no aluno selecionado, passar as configurações o arquivo de histórico que foi gerado por insert_escolas preenchido,
+e o estudante referente ao histórico
+"""
+
+from bson.objectid import ObjectId
+from pymongo import MongoClient
+from urllib.parse import quote_plus
+import json
+import copy
+import os
+import argparse
+from functools import reduce
+
+def convert_historico_id(historico):
+    historico['disciplina_id'] = ObjectId(historico['disciplina_id']) 
+    return historico
+
+
+local_path = os.path.dirname(__file__)
+print("Local path",local_path)
+db_resources_path = os.path.join(local_path, '..', '..', 'db_resources')
+
+# Normalizando o caminho
+db_resources_path = os.path.abspath(db_resources_path)
+
+parser = argparse.ArgumentParser(description="Insere escola no banco de dados.")
+
+#Recupera argumentos
+parser.add_argument("--record", type=str, default="", help="Historico Escolar")
+parser.add_argument("--env", type=str, default="", help="Ambiente")
+parser.add_argument("--student", type=str, default="", help="Estudante")
+
+# Carrega configurações
+args = parser.parse_args()
+f_config = {}
+f_config = open('{}/db_config.json'.format(db_resources_path))
+mongo_db_infos = json.load(f_config)
+
+mongo_db_infos = mongo_db_infos[args.env]
+
+if 'CLUSTER' not in mongo_db_infos:
+    mongo_db_infos['CLUSTER'] = mongo_db_infos['HOST'] + ":" + mongo_db_infos['PORT']
+
+# Connect to the database
+connection_string = 'mongodb{}://{}:{}@{}/{}'.format(
+            mongo_db_infos['SRV'],
+            mongo_db_infos['USER'],
+            quote_plus(mongo_db_infos['PASSWORD']),
+            mongo_db_infos['CLUSTER'],
+            mongo_db_infos['PARAMS']
+        )
+client = MongoClient(connection_string)
+db = client['competencias_enem_data']
+collection = db['escolas']
+historico={}
+with open('{}/data/{}.json'.format(local_path, args.record), "r", encoding="utf-8") as file:
+    historico = json.loads(file.read())
+
+#Recupera escola associada ao estudante
+student = args.student
+query = {"turmas.alunos.nome": student}
+results = []
+
+for key,value in historico.items():
+
+    result = collection.find_one({'turmas.serie':key})
+
+    historico_escolar = list(map(convert_historico_id,value))
+   
+    # # Atualização para modificar o histórico do aluno específico
+    update = {
+        "$set": {
+            "turmas.$[turma].alunos.$[aluno].notas.historico_escolar": historico_escolar
+        }
+    }
+    # # Opções para identificar o aluno e a turma corretos
+    array_filters = [
+        {"turma.serie": key},  # Filtra a turma correta
+        {"aluno.nome": args.student}  # Filtra o aluno correto
+    ]
+
+    # # Executar a atualização
+    results.append(collection.update_one(query, update, array_filters=array_filters))
+
+print(reduce(lambda acc, result: acc + result.modified_count,results,0))
+print("Inserção realizada, {}/3 históricos por serie atualizados")
